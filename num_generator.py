0c82cd3bfc2b1fd4e316bfa7b7b33635ffcc92c1
Devan Veach <dazetrading0880@gmail.com>
2025-04-04 01:27:44 -0500

    added comments to num_generat.py

--- num_generator.py
+++ num_generator.py
@@ -3,11 +3,13 @@ import json
 
 class Num_Generator:
     def __init__(self):
+        '''initalize class resources'''
         with open('database.json', 'r') as file:
             data = json.load(file)
             self.count = data['count']
 
     def get_random_number(self):
+        '''generates a random number, saves new data and calls function to update count'''
         number = int(np.random.randint(1, 100, size=1)[0])
         self.count += 1
         new_data = {
@@ -17,5 +19,6 @@ class Num_Generator:
         self.update_count(new_data)
     
     def update_count(self, new_data):
+        '''updates count in database'''
         with open('database.json', 'w') as file:
             json.dump(new_data, file, indent=4)
